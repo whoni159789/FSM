8b21502f66032f1e8d503bdb898b10093e8e8350
whoni159789 <whoni6131@gmail.com>
2022-10-13 15:26:47 +0900

    Ver 4. Button_DeBounce

--- sources_1/new/ButtonController.v
+++ sources_1/new/ButtonController.v
@@ -11,9 +11,13 @@ module ButtonController(
                 RELEASED = 1'b0,
                 TRUE = 1'b1,
                 FALSE = 1'b0;
+    
+    parameter   DEBOUNCE = 1_000_000; // 10msec (10ns * 1,000,000 = 10,000,000us = 10msec)
 
     reg r_prevState = RELEASED;
     reg r_button;
+    reg [31:0] r_counter = 0;
+
     assign o_button = r_button;
 
 
@@ -21,19 +25,31 @@ module ButtonController(
     /* 비동기식 방식 */
     always @(posedge i_clk or posedge i_reset) begin
         if(i_reset) begin
-            r_button <= RELEASED;
+            r_button <= FALSE;
+            r_prevState <= RELEASED;
+            r_counter <= 0;
         end
         else begin
-            if(i_button == PUSHED && r_prevState == RELEASED) begin
-                r_prevState = PUSHED;
-                r_button = FALSE;
+            if(i_button == PUSHED && r_prevState == RELEASED && r_counter < DEBOUNCE) begin
+                r_counter <= r_counter + 1;
+                r_button <= FALSE;
             end
-            else if(i_button == RELEASED && r_prevState == PUSHED) begin
-                r_prevState = RELEASED;
-                r_button = TRUE;
+            else if (i_button == PUSHED && r_prevState == RELEASED && r_counter == DEBOUNCE) begin
+                r_counter <= 0;
+                r_prevState <= PUSHED;
+                r_button <= FALSE;
+            end
+            else if (i_button == RELEASED && r_prevState == PUSHED && r_counter < DEBOUNCE) begin
+                r_counter <= r_counter + 1;
+                r_button <= FALSE;
+            end
+            else if(i_button == RELEASED && r_prevState == PUSHED && r_counter == DEBOUNCE) begin
+                r_counter <= 0;
+                r_prevState <= RELEASED;
+                r_button <= TRUE;
             end
             else begin
-                r_button = FALSE;
+                r_button <= FALSE;
             end
         end
     end
